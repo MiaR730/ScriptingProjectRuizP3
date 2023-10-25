a99cae31ed44438439f3a0498aa3254ea5bcaac7
MiaR730 <723420@student.dusd.net>
2023-10-25 11:21:58 -0700

    Added game object

--- ScriptingProjectP3Ruiz/Assets/Scripts/Varibles.cs
+++ ScriptingProjectP3Ruiz/Assets/Scripts/Varibles.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Varibles : MonoBehaviour
 {
+    int myInt = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
     }
 }
